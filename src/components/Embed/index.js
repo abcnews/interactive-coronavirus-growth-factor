6ff5f53f352e0a982ce64b0d1603d09b0a119d61
Simon Elvery <simon@elvery.net>
2020-04-10 00:03:40 +1000

    Add link to embed

--- src/components/Embed/index.js
+++ src/components/Embed/index.js
@@ -21,14 +21,21 @@ export const Embed = ({ jurisdiction, data, smoothing = 5 }) => {
         We need to keep growth factor <strong>below 1.0</strong> to stop the
         exponential growth of COVID-19 cases
       </h1>
-      <CurrentLabel labelText={labelText} value={currentGrowthFactor} />
+      <CurrentLabel
+        labelText={labelText}
+        value={currentGrowthFactor}
+        first="true"
+      />
       <GrowthFactorChart
         data={series.slice(-30)}
         height={200}
         innerHeight={60}
       />
       <Extremes data={series} emphasise={true} className={styles.extremes} />
-      <a className={styles.more} href="">
+      <a
+        className={styles.more}
+        href="https://www.abc.net.au/news/2020-04-09/coronavirus-data-australia-growth-factor-covid-19/12132478"
+      >
         Find out more
       </a>
     </div>
